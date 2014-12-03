7788646a0edc30f60cfd3934683d29d4f224fc0d
Rajeev Guru <guru.rajeev@gmail.com>
2014-12-03 01:16:41 +0100

    latest version of the looks app

--- client/app/admin/upload/upload.controller.js
+++ client/app/admin/upload/upload.controller.js
@@ -3,6 +3,12 @@
 angular.module('thelooksappApp')
   .controller('UploadCtrl', function ($scope) {
 
+    $scope.tags=[];
+
+    $scope.uploadedfiles=[];
+
+    $scope.isUploading = false;
+
     $scope.dropzoneConfig = {
       'options': { // passed into the Dropzone constructor
         'url': 'https://api.cloudinary.com/v1_1/www-thelooks-net/image/upload'
@@ -12,6 +18,12 @@ angular.module('thelooksappApp')
       'eventHandlers': {
         'sending': function (file, xhr, formData) {
           formData.append("upload_preset","wefi1sgv");
+          if ($scope.isUploading == false){
+            $scope.isUploading = true;
+            $scope.$apply();
+          }
+
+
         },
         'success': function (file, response) {
 
@@ -30,11 +42,22 @@ angular.module('thelooksappApp')
 
         'dragleave' : function(event) {
           console.log("dragleave event" +  event);
+        },
+
+        'queuecomplete' : function(e) {
+          console.log('completed!');
+
+          $scope.isUploading = false;
+          $scope.$apply()
         }
+
+
 
 
 
       }
     };
+
+
 
   });
